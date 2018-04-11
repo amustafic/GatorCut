78d43a90591bc855975a3f2e75de9e17517d8602
jensenrrr <32421710+jensenrrr@users.noreply.github.com>
2018-04-10 21:11:44 -0400

    Included Asia's getDist
    Makes the getDist function compatible with the locations Node* vector.

--- main.h
+++ main.h
@@ -14,6 +14,17 @@ public:
 	Node(int x, int y, string name);
 };
 
+double getDist(Node* c,Node* d){
+    double x1= c->getX();
+    double x2=d->getX();
+    double y1=c->getY();
+    double y2=d->getY();
+
+    double xDiff = x1-x2;
+    double yDiff = y1-y2;
+    double dist= sqrt(pow(xDiff,2)+pow(yDiff,2));
+    return dist;
+}
 
 /*void PrintMenu(*Node [] locations);
 void addNodeLocation(string name, int x, int y);
