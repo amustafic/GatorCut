54bc4bc463862516733a704fe35b54d565d9c2b6
jensenrrr <32421710+jensenrrr@users.noreply.github.com>
2018-04-12 14:40:42 -0400

    Updated along with main.cpp
    reflects changes to the Node class with the two member variable vectors and uses an overload getDist method to help with the add Node implementation

--- main.h
+++ main.h
@@ -10,12 +10,18 @@ private:
 	double lon;
 	double lat;
 	string name;
+	vector <Node*> neighbors;
+	vector <double> distances;
 
 public:
 	string getName();
 	double getLon();
 	double getLat();
-	Node(int lon, int lat, string name);
+	vector <Node*> getNeighbors();
+	vector <double> getDistances();
+	void pushNeighbors(Node* neighbor);
+	void pushDistances(double distance);
+	Node(int lon, int lat, string name, vector <Node*> neighbors, vector <double> distances);
 };
 
 double getDist(Node* firstNode,Node* secondNode){
@@ -28,3 +34,18 @@ double getDist(Node* firstNode,Node* secondNode){
   	double d = c * 6371000;
     return d;
 }
+
+double getDist(double lonOne, double latOne, Node* secondNode){
+	double deltaLat = latOne - secondNode->getLat();
+  	double deltaLon = lonOne - secondNode->getLon();
+  	//haversine formula
+  	double a = sin(deltaLat*PI/360)*sin(deltaLat*PI/360) + cos(latOne) * cos(secondNode->getLat()) * sin(deltaLon*PI/360)*sin(deltaLon*PI/360);
+  	double c = 2 * atan2(sqrt(a),sqrt(1-a));
+  	double d = c * 6371000;
+    return d;
+}
+
+/*void PrintMenu(*Node [] locations);
+void addNodeLocation(string name, int x, int y);
+string [] Dijkstra(*Node [] locations);
+*/
