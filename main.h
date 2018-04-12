e40d8b39524f6218035aa7c4ef7adccc48b83b77
jensenrrr <32421710+jensenrrr@users.noreply.github.com>
2018-04-12 03:46:38 -0400

    Updated main.h, getDist for long lat
    getDist works with longitude and latitude, not x and y, distance returned is currently in kilometers.

--- main.h
+++ main.h
@@ -1,16 +1,30 @@
+#include <string>
+#include <math.h>
+#define PI 3.14159265
+
+using namespace std;
+
 class Node{
 
 private:
-	int x;
-	int y;
+	double lon;
+	double lat;
 	string name;
 
 public:
 	string getName();
-	int getX();
-	int getY();
-}
+	double getLon();
+	double getLat();
+	Node(int lon, int lat, string name);
+};
 
-void PrintMenu(*Node [] locations);
-void addNodeLocation(string name, int x, int y);
-string [] Dijkstra(*Node [] locations);
+double getDist(Node* firstNode,Node* secondNode){
+  
+  	double deltaLat = firstNode->getLat() - secondNode->getLat();
+  	double deltaLon = firstNode->getLon() - secondNode->getLon();
+  	//haversine formula
+  	double a = sin(deltaLat*PI/360)*sin(deltaLat*PI/360) + cos(firstNode->getLat()) * cos(secondNode->getLat()) * sin(deltaLon*PI/360)*sin(deltaLon*PI/360);
+  	double c = 2 * atan2(sqrt(a),sqrt(1-a));
+  	double d = c * 6371000;
+    return d;
+}
